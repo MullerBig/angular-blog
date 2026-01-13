6524aa6478fcb16767ebf8d88bfa618fd18f7408
Geovane <geovane.desouzaferreira07@gmail.com>
2026-01-12 22:54:05 -0300

    componentes criados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitle } from "./components/menu-title/menu-title";
+import { MenuBar } from "./components/menu-bar/menu-bar";
+import { BigCard } from "./components/big-card/big-card";
+import { SmallCard } from "./components/small-card/small-card";
+import { Home } from "./pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
